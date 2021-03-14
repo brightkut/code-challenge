9b706add944d6c10561b85b0fa2ec34512d67c4d
disornthitikornkovit <dsorn2@gmail.com>
2021-03-14 14:15:11 +0700

    - pass roman to integer

---
+++ src/roman_to_integer_13/Main.java
@@ -0,0 +1,43 @@
+package roman_to_integer_13;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        //III
+        //IV
+        //LVIII
+        //MCMXCIV
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("IV"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
+    }
+
+    public static int romanToInt(String s) {
+        HashMap<Character,Integer> romanMap = new HashMap<>();
+        romanMap.put('I',1);
+        romanMap.put('V',5);
+        romanMap.put('X',10);
+        romanMap.put('L',50);
+        romanMap.put('C',100);
+        romanMap.put('D',500);
+        romanMap.put('M',1000);
+
+        if(s.length()==1){
+            return romanMap.get(s.charAt(0));
+        }
+
+        int sum = romanMap.get(s.charAt(s.length()-1));
+
+        for(int i =0;i<s.length()-1;i++){
+         int value1 = romanMap.get(s.charAt(i));
+         int value2 = romanMap.get(s.charAt(i+1));
+
+         if(value1>=value2)sum+=value1;
+         else sum-=value1 ;
+        }
+
+        return sum;
+    }
+}
