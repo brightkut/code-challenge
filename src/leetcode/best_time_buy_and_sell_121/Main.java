af1b7e559081b50429c0223f91c57cf7ea3c8f6a
disornthitikornkovit <dsorn2@gmail.com>
2022-08-14 20:16:54 +0700

    - add new leet code problem (917)

---
+++ src/leetcode/best_time_buy_and_sell_121/Main.java
@@ -0,0 +1,43 @@
+package leetcode.best_time_buy_and_sell_121;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    //brute force O(n2)
+    public int maxProfit(int[] prices) {
+
+        //find the minimum and buy max after min index
+        int min = Integer.MAX_VALUE;
+        for (int i =0 ; i< prices.length ; i++){
+            for(int j = i ; j< prices.length; j++){
+                if(prices[i]-prices[j]< min) min =prices[i]-prices[j];
+            }
+        }
+
+        return Math.abs(min);
+    }
+
+     public static int maxProfit2(int [] prices){
+        //buy when min and sell max
+
+         int max = Integer.MAX_VALUE;
+         int min = 0;
+
+         for (int i =0 ; i< prices.length ; i++){
+             if(max >  prices[i]) max = prices[i];
+
+             int profit = prices[i] - max;
+
+             if(profit > min){
+                 min = profit;
+             }
+         }
+
+         return min;
+     }
+}
---
+++ src/leetcode/pascal_triangle_118/Main.java
@@ -0,0 +1,32 @@
+package leetcode.pascal_triangle_118;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> r,pre = null;
+
+        for(int i=0; i<numRows;i++){
+            r = new ArrayList<>();
+            for(int j=0 ; j<=i ;j++){
+                if(j==0 || j == i){
+                    r.add(1);
+                }else{
+                    r.add(pre.get(j-1)+pre.get(j));
+                }
+            }
+            pre = r;
+            res.add(r);
+        }
+
+        return res;
+    }
+
+
+}
