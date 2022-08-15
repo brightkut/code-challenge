f6ee2490d3a1181dba8f3380751ee57a13f04a18
disornthitikornkovit <dsorn2@gmail.com>
2022-08-15 20:29:31 +0700

    - add new leet code problem (209)

---
+++ src/leetcode/minimum_size_subarray_sum_209/Main.java
@@ -0,0 +1,33 @@
+package leetcode.minimum_size_subarray_sum_209;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int [] nums = {2,3,1,2,4,3};
+        System.out.println(minSubArrayLen(7,nums));
+    }
+
+    public static int minSubArrayLen(int target, int[] nums) {
+
+        int curVal = 0;
+        int c = 0;
+        int min = Integer.MAX_VALUE;
+        int p = 0;
+
+        for (int i =0 ; i< nums.length;i++){
+            curVal+=nums[i];
+            c++;
+
+            while (curVal>=target){
+                min = Math.min(c,min);
+                curVal -= nums[p];
+                p++;
+            }
+        }
+
+        return min == Integer.MAX_VALUE ?  0 : min;
+    }
+}
